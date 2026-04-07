4f5716eebc3194d1a29f1b1001b78b948a93d018
Leanne Brown <leannesarahbrown@gmail.com>
2026-04-06 20:36:38 -0400

    modal changes

--- logs.js
+++ logs.js
@@ -1,14 +1,32 @@
+const openBtn = document.getElementById("open-log-modal");
+const modal = document.getElementById("log-modal");
+const cancelBtn = document.querySelector(".cancel-training-button");
+const modalForm = document.getElementById("training-form");
 
-//user clicks log training button
+let logs = JSON.parse(localStorage.getItem("logs")) || [];
+
+openBtn.addEventListener("click", () => {
+    modal.classList.add("show");
+});
+
+cancelBtn.addEventListener("click", () => {
+ modal.classList.remove("show");
+});
+
+modal.addEventListener("click", (e) => {
+  if(e.target === modal) {
+    modal.classList.remove("show");
+  }
+});
+
+modalForm.addEventListener("submit", (e) => {
+
+    //user inputs data
 
 
-//form appears in center of page
+    //save info as an object
 
 
-//background dims
-
-
-//user inputs data
-
-
-//user saves or cancels
+    //save to local storage 
+    
+});
